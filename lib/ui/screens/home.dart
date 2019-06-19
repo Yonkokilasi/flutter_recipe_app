167d2636db6afed4d1de9cfa54ca10b02c6aa834
kilasi <joseph.kilasi@incentro.com>
2019-06-19 15:08:59 +0300

    added bottom tab bar clicking on signin buttons now navigates to home page

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -10,14 +10,26 @@ class HomeScreen extends StatelessWidget {
       length: 4,
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
+          backgroundColor: Colors.deepOrange,
           elevation: 2.0,
           bottom: TabBar(
             labelColor: Theme.of(context).indicatorColor,
             tabs: [
-              Tab(
-                icon: Icon(Icons.restaurant, size: _iconSize),
-              )
+              Tab(icon: Icon(Icons.restaurant, size: _iconSize)),
+              Tab(icon: Icon(Icons.local_drink, size: _iconSize)),
+              Tab(icon: Icon(Icons.favorite, size: _iconSize)),
+              Tab(icon: Icon(Icons.settings, size: _iconSize))
+            ],
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(5.0),
+          child: TabBarView(
+            children: [
+              Center(child: Icon(Icons.restaurant)),
+              Center(child: Icon(Icons.local_drink)),
+              Center(child: Icon(Icons.favorite)),
+              Center(child: Icon(Icons.settings))
             ],
           ),
         ),
