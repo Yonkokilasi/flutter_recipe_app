faaa2adc2ea03afe482abef9a388b8e25d2801d1
kilasi <joseph.kilasi@incentro.com>
2019-06-25 14:31:14 +0300

    now displaying data in each tab  based on logic

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,9 +1,47 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:recipes_app/model/recipe.dart';
+import 'package:recipes_app/utils/store.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  List<Recipe> recipes = getRecipes();
+  List<String> userFavorites = getFavoritesIDs();
+
+  // favorite or unfavorite recipe
+  void _handleFavoriteListChanged(String recipeID) {
+    setState(() {
+      if (userFavorites.contains(recipeID)) {
+        userFavorites.remove(recipeID);
+      } else {
+        userFavorites.add(recipeID);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Column _buildRecipes(List<Recipe> recipeList) {
+      return Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: recipeList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(recipeList[index].name),
+                );
+              },
+            ),
+          )
+        ],
+      );
+    }
+
     double _iconSize = 20.0;
 
     return DefaultTabController(
@@ -29,10 +67,21 @@ class HomeScreen extends StatelessWidget {
           padding: EdgeInsets.all(5.0),
           child: TabBarView(
             children: [
-              Center(child: Icon(Icons.restaurant)),
-              Center(child: Icon(Icons.local_drink)),
-              Center(child: Icon(Icons.favorite)),
-              Center(child: Icon(Icons.settings))
+              // display recipe type food
+              _buildRecipes(recipes
+                  .where((recipe) => recipe.type == RecipeType.food)
+                  .toList()),
+
+              // display recipes type drink
+              _buildRecipes(recipes
+                  .where((recipe) => recipe.type == RecipeType.drink)
+                  .toList()),
+
+              // display recipes
+              _buildRecipes(recipes
+                  .where((recipe) => userFavorites.contains(recipe.id))
+                  .toList()),
+              Center(child: Icon(Icons.settings)),
             ],
           ),
         ),
